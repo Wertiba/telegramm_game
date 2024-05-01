e01857e376d2bf46b878f0636f4faca5f6593a48
wertiba <wertibarozen@gmail.com>
2024-05-01 19:05:33 +0300

    add files and do working code

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+#import modules
+import random
 
-print('hello world')
-print('egor')
+
+
+#create class for races
+class Races():
+    def __init__(self, name, title, number, power, health):
+        self.name = name
+        self.title = title
+        self.number = number
+        self.power = power
+        self.health = health
+
+
+vikings = Races('викинги', 'just_title', 'just_number', '650', '450')
+peoples = Races('современники', 'just_title', 'just_number', '800', '300')
+nigers = Races('негры', 'just_title', 'just_number', '600', '800')
+polars = Races('полярники', 'just_title', 'just_number', '600', '600')
+fire_regiments = Races('огнеполки', 'just_title', 'just_number', '850', '200')
+
+races_list = [vikings, peoples, nigers, polars, fire_regiments]
