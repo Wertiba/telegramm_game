55fee79e0e2a3afead439c8697a13c8d8c2bfccc
wertiba <wertibarozen@gmail.com>
2024-05-01 19:21:55 +0300

    add telebot settings and command start

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telebot import types
 from icecream import ic
 
 #create bot
-bot = telebot.TeleBot('6042698676:AAEfUDOR2v2KqOkxRfpC6s1qn6hYpoU7Pdk')
+bot = telebot.TeleBot('7177989754:AAFO8VfvNuLfa7hVyszlwZjz6UEL-kJ6I_s')
 #disable ic
 ic.disable()
 
@@ -26,4 +26,17 @@ nigers = Races('негры', 'just_title', 'just_number', '600', '800')
 polars = Races('полярники', 'just_title', 'just_number', '600', '600')
 fire_regiments = Races('огнеполки', 'just_title', 'just_number', '850', '200')
 
+#list with all races
 races_list = [vikings, peoples, nigers, polars, fire_regiments]
+
+#function for command start
+@bot.message_handler(commands=['start'])
+def start(message):
+    murkup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('x')
+    btn2 = types.KeyboardButton('y')
+    murkup.row(btn1, btn2)
+
+    bot.send_message(message.chat.id, f'Здравствуйте, {message.from_user.first_name}', reply_markup=murkup)
+
+bot.polling(none_stop=True)
